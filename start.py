b6573a4c83fd2ad149f30b2c3503679bf29c6e03
Anton <antohin87@ya.ru>
2020-09-30 08:57:03 +0700

    Переписан код

--- start.py
+++ start.py
@@ -1,17 +1,20 @@
 import urllib.request
-from _ast import Is
-
 from bs4 import BeautifulSoup
 
+
 class Scraper:
-    def __init__(self,site):
-        self.site =site
+    def __init__(self, site):
+        self.site = site
 
     def scrape(self):
-        r=urllib.request.urlopen(self.site)
-        html=r.read()
-        parser="html.parser"
-        sp = BeautifulSoup(html, parser)
+        response = urllib.request.urlopen(self.site)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        with open("output.txt", "w") as f:
+            for tag in soup.find_all('a'):
+                url = tag.get('href')
+                if url and 'html' in url:
+                    print("\n" + url)
+                    f.write(url + "\n")
 
-new = Scraper("https://news.google.ru/")
-new.scrape()
+Scraper('https://news.google.ru/').scrape()
